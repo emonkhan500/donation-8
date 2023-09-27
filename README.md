bd7317722d9e6eef75e906f937a9ff96dbedcb7b
emonkhan500 <khdilo47@gmail.com>
2023-09-27 17:04:59 +0600

    pi , readmi add and full complete

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
-# React + Vite
+# Here is my website features
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+- This website is for collecting donation fund for poor people. 
+- You can donate four categories in this website
+- You can see your donation in Donation page.
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/Components/Chart/Pi.jsx
+++ src/Components/Chart/Pi.jsx
@@ -1,58 +1,68 @@
-
-import React, { useState, useEffect } from 'react';
-import { PieChart, Pie, Cell, Tooltip } from 'recharts';
-
+import  { useState, useEffect } from 'react';
+import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
 import { getClickDonation } from '../../Utilitis/Utilitis';
+
 
 const Pi = () => {
   const [localStorageData, setLocalStorageData] = useState([]);
 
-  // Fetch the stored donation data from localStorage when the component mounts
+  
   useEffect(() => {
     const storedDonationAmounts = getClickDonation();
     setLocalStorageData(storedDonationAmounts);
   }, []);
 
   const totalDonations = 12;
-  const dynamicDonations = localStorageData.length; // Length of data from localStorage
+  const dynamicDonations = localStorageData.length; 
   const remainingDonations = totalDonations - dynamicDonations;
 
-  // Calculate the percentages
+  const RADIAN = Math.PI / 180;
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    return (
+      <text x={x} y={y} fill="white" fontSize={24} fontWeight={'bold'} textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    );
+  };
+  
+  
   const totalDonationsPercentage = (remainingDonations / totalDonations) * 100;
   const dynamicDonationsPercentage = (dynamicDonations / totalDonations) * 100;
 
   const data = [
-    { name: 'Total Donations', value: totalDonations },
-    { name: 'Dynamic Donations', value: dynamicDonations },
+    { name: 'Your Donation', value: dynamicDonationsPercentage },
+    { name: 'Remaining Donation', value: totalDonationsPercentage },
   ];
 
-  const colors = ['#FF6384', '#36A2EB'];
+  const colors = ['#FF444A', '#00C49F'];
 
   return (
     <div className='container mx-auto'>
-      <PieChart width={1380} height={400}>
+      
+      <PieChart width={1400} height={600}>
         <Pie
           dataKey="value"
           isAnimationActive={true}
           data={data}
-          cx={750}
-          cy={200}
-          outerRadius={150}
+          cx={700}
+          cy={220}
+          outerRadius={185}
           fill="#8884d8"
-          label={false}
+          labelLine={false} 
+          label={renderCustomizedLabel}
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
           ))}
         </Pie>
         <Tooltip />
+        <Legend verticalAlign="bottom" height={120} />
       </PieChart>
-      <p className='text-center'>
-        Total Donations Percentage: {totalDonationsPercentage.toFixed(2)}%
-      </p>
-      <p className='text-center'>
-        Dynamic Donations Percentage: {dynamicDonationsPercentage.toFixed(2)}%
-      </p>
+      
     </div>
   );
 };
